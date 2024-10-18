18af5551d43abfad49b199e45f6e8760792937b8
hebertaragao <hebert.aragao@outlook.com>
2024-10-18 15:36:48 -0300

    creating the victory condition

--- memory_game_witch_emojis-js/src/scripts/engine.js
+++ memory_game_witch_emojis-js/src/scripts/engine.js
@@ -38,5 +38,19 @@ for (let i = 0; i < emojis.length; i++) {
     }
   }
 function checkMatch(){
+    if(openCards[0].innerHTML === openCards[1].innerHTML){
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    }else{
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+    }
+
+    openCards = [];
+
+    if(document.querySelectorAll("boxMatch").length ==  emojis.length){
+        alert("Você venceu!");
+    }
+
     
 }
