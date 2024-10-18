681cf7eb82ac69dbd66add4edd1e878bfb471993
hebertaragao <hebert.aragao@outlook.com>
2024-10-18 14:48:02 -0300

    update engine.js

---
+++ memory_game_witch_emojis-js/src/scripts/engine.js
@@ -0,0 +1,28 @@
+const emojis = [
+    "✋",
+    "✋",
+    "👀",
+    "👀",
+    "👭",
+    "👭",
+    "💜",
+    "💜",
+    "🕺",
+    "🕺",
+    "🚴",
+    "🚴",
+    "🤸",
+    "🤸",
+];
+
+let openCards = [];
+
+
+let shuffleEmojis = emojis.sort(() => (Math.random()>0.5 ? 2 : -1))
+
+for (let i = 0; i < emojis.length; i++) {
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector(".game").appendChild(box);
+  }
