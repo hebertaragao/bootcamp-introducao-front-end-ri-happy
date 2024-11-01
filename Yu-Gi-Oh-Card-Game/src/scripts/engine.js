aab2375dd0b226222ed898a217a3eff47219da6a
hebertaragao <hebert.aragao@outlook.com>
2024-11-01 08:36:58 -0300

    implementing dueling logic

--- Yu-Gi-Oh-Card-Game/src/scripts/engine.js
+++ Yu-Gi-Oh-Card-Game/src/scripts/engine.js
@@ -104,6 +104,32 @@ async function setCardField(cardId){
     await drawButton(duelResults);
 }
 
+async function drawButton(text){
+    state.actions.button.innerText = text;
+    state.actions.button.style.display = "block";
+}
+
+async function updateScore() {
+    state.score.scoreBox.innerText = `Win: ${state.score.playerScore} | Lose: ${state.score.computerScore}`;
+}
+
+async function checkDueResults(playerCardId, computerCardId){
+    let duelResults = "Empate";
+    let playerCard = cardData[playerCardId];
+
+    if(playerCard.WinOf.includes(computerCardId)){
+        duelResults = "Ganhou"
+        state.score.playerScore++;
+    }
+
+    if(playerCard.LoseOf.includes(computerCardId)){
+        duelResults = "Perdeu"
+        state.score.computerScore++;
+    }
+
+    return duelResults;
+}
+
 async function removeAllCardsImages(){ 
     let {computerBOX, player1BOX} = state.playerSides;
     let imgElements = cards.querySelectorAll("img")
