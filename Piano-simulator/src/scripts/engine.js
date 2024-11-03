71970af399a8fa92df40b711553bee83639aa896
hebertaragao <hebert.aragao@outlook.com>
2024-11-03 15:13:28 -0300

    fixing the code

--- Piano-simulator/src/scripts/engine.js
+++ Piano-simulator/src/scripts/engine.js
@@ -1,45 +1,40 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
+const volumeSlider = document.querySelector(".volume-slider input");
+const keysCheck = document.querySelector(".keys-check input");
 
-const volumeSlider = document.querySelectorAll(".volume-slider input")
-
-const keysCheck = document.querySelectorAll(".keys-check input")
-
-let mappedKeys = [];
-
+let mapedKeys = [];
 let audio = new Audio("src/tunes/a.wav");
 
 const playTune = (key) => {
   audio.src = `src/tunes/${key}.wav`;
-  audio.play()
+  audio.play();
 
-  const clickedKey = document.querySelectorAll(`[data-key = "${key}"]`)
-  clickedKey.classList.add("activite")
-  setTimeout(()=>{clickedKey.classList.remove("activite")},150)
+  const clickedKey = document.querySelector(`[data-key="${key}"]`);
+  clickedKey.classList.add("active");
+  setTimeout(() => {
+    clickedKey.classList.remove("active");
+  }, 150);
 };
 
-  pianoKeys.forEach((key) => {
-    key.addEventListener("click", () => playTune(key.dataset.key));
-    mapedKeys.push(key.dataset.key);
-    mappedKeys.push(key.dataset.key)
-  });
+pianoKeys.forEach((key) => {
+  key.addEventListener("click", () => playTune(key.dataset.key));
+  mapedKeys.push(key.dataset.key);
+});
 
-  document.addEventListener("keydown", (e) => {playTune(e.key); 
-
-    if(mappedKeys.includes(e.key)){
-        playTune(e.key)
-    }
-  
+document.addEventListener("keydown", (e) => {
+  if (mapedKeys.includes(e.key)) {
+    playTune(e.key);
+  }
 });
 
 const handleVolume = (e) => {
-    audio.volume = e.target.value;
+  audio.volume = e.target.value;
+};
 
-}
+const showHideKeys = () => {
+  pianoKeys.forEach((key) => key.classList.toggle("hide"));
+};
 
-const showHideKeys = ( ) => {
-    pianoKeys.forEach(key => key.classList.toggle("hidde"))
-}
+volumeSlider.addEventListener("input", handleVolume);
 
-volumeSlider.addEventListener("input", handleVolume)
-
-keysCheck.addEventListener("click", showHideKeys)
+keysCheck.addEventListener("click", showHideKeys);
