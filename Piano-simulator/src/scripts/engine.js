7f7f229d2c53fdbd48cb01090e9607bd2bd5e32e
hebertaragao <hebert.aragao@outlook.com>
2024-11-03 14:49:53 -0300

    Mapping allowed keys

--- Piano-simulator/src/scripts/engine.js
+++ Piano-simulator/src/scripts/engine.js
@@ -1,5 +1,7 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
+let mappedKeys = [];
+
 let audio = new Audio("src/tunes/a.wav");
 
 const playTune = (key) => {
@@ -14,7 +16,13 @@ const playTune = (key) => {
   pianoKeys.forEach((key) => {
     key.addEventListener("click", () => playTune(key.dataset.key));
     mapedKeys.push(key.dataset.key);
+    mappedKeys.push(key.dataset.key)
   });
 
   document.addEventListener("keydown", (e) => {playTune(e.key); 
+
+    if(mappedKeys.includes(e.key)){
+        playTune(e.key)
+    }
+  
 });
